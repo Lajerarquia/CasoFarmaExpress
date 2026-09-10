56359a2bb5263f4044134c35b272b985f29362ce
Servicios Digitales Concepcion <cetecom_conce@duoc.cl>
2026-09-10 16:39:18 -0300

    Inicializa microservicio de prescripciones

---
+++ ms-farmaexpress-prescriptions/src/main/java/cl/duoc/farmaexpress/prescriptions/MsFarmaexpressPrescriptionsApplication.java
@@ -0,0 +1,13 @@
+package cl.duoc.farmaexpress.prescriptions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsFarmaexpressPrescriptionsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsFarmaexpressPrescriptionsApplication.class, args);
+	}
+
+}
---
+++ ms-farmaexpress-prescriptions/src/test/java/cl/duoc/farmaexpress/prescriptions/MsFarmaexpressPrescriptionsApplicationTests.java
@@ -0,0 +1,13 @@
+package cl.duoc.farmaexpress.prescriptions;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsFarmaexpressPrescriptionsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
